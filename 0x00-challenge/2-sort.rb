5ff5d2a34600e67bf538a3ee1f9ba3331a3dcfe9
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-01-29 02:32:03 +0200

    try to fix the code

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -14,13 +14,14 @@ ARGV.each do |arg|
 
     # insert result at the right position
     is_inserted = false
+
     i = 0
     l = result.size
     while !is_inserted && i < l do
         if result[i] < i_arg
             i += 1
         else
-            result.insert(i, i_arg) # Fix: insert at index i instead of i - 1
+            result.insert(i, i_arg)
             is_inserted = true
             break
         end
