681a0568bcdea521deed2f658f506856bd94c586
Abdelrahman hassan <abdelrahman_hassan31@yahoo.com>
2024-03-20 04:44:21 +0200

    fix square class

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
+"""This module creates a class square"""
 
-class square():
-    
+
+class Square():
+    """This class creates a square object"""
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
+        """Initializes the square object"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
+        """Calculates the area of the square"""
         """ Area of the square """
         return self.width * self.width
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
+        """Calculates the perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """Returns the string representation of the square object"""
         return "{}/{}".format(self.width, self.height)
 
-if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+if __name__ == "__main__":
+    """This is executed when the module is run as a script"""
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
